6daeb7f09859dae4a805a7f8604676424f1acfa7
itswizardexpert <90338417+itswizardexpert@users.noreply.github.com>
2022-08-23 11:30:05 -0400

    Add files via upload

---
+++ MadLib.py
@@ -0,0 +1,4 @@
+Color = print("enter a color") + input()
+Place = print("enter a place") + input()
+Car = print("enter a car brand") + input()
+print("your favorite color is" + Color, "A place you would recomand to a traveler would be " + Place, "And a good car would be a "+ Car)
